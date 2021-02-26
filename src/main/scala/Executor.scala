25d89ab485c1f43aef76785e15ce4fb06edf54fc
Shogo Takegawa <b6151@oic.jp>
2021-02-26 17:54:57 +0900

    insert書いた

--- src/main/scala/Executor.scala
+++ src/main/scala/Executor.scala
@@ -1,3 +1,5 @@
+import Table.Row
+
 object Executor {
 
   sealed trait Result
@@ -7,25 +9,17 @@ object Executor {
   case object Success extends Result
 
   def execute(statement: Statement)(implicit table: Table): Result = statement.sType match {
-    case Statement.Insert(_) =>
-      println("This is where we would do an insert.")
-      executeInsert(statement, table)
+    case Statement.Insert(rowToInsert) =>
+      executeInsert(rowToInsert, table)
     case Statement.Select =>
       executeSelect(statement, table)
   }
 
-  private def executeInsert(statement: Statement, table: Table): Result = {
-    if (table.numRows >= Table.TABLE_MAX_ROWS) {
-      TableFull
-    } else {
-      // MEMO:
-      // 現状 val で table を引き回しているため、変更が不可能
-      // Immutable に table を取り回したい場合は、戻り値などの変更も必要
-      // とはいえ、データベース全体を Immutable に取りまわすってどうよ 😇
-
-      Success
+  private def executeInsert(row: Row, table: Table): Result =
+    table.insert(row) match {
+      case Left(_)  => TableFull
+      case Right(_) => Success // TODO Table引き回す
     }
-  }
 
   private def executeSelect(statement: Statement, table: Table): Result = {
     table.pages.flatten.foreach(println)
--- src/main/scala/Table.scala
+++ src/main/scala/Table.scala
@@ -4,27 +4,32 @@ case class Table(
     numRows: Long,
     pages: Vector[Page]
 ) {
-  // MEMO:
-  // numRows ≠ numPages
-  // 一旦データの追加は度外視して、空の Table を表示させてみる
 
-  //def +(row: Row): Either[RuntimeException, Table] = {
-  //  if (numRows >= 100) {
-  //    Left(new RuntimeException("exceed page size"))
-  //  } else {
-  //  Right(new Table(numRows + 1, pages :+ row))
-  //  }
-  //}
+  import Table._
+
+  def insert(row: Row): Either[RuntimeException, Table] =
+    if (numRows < TABLE_MAX_ROWS) {
+      val newPages: Vector[Page] =
+        if (pages.lastOption.exists(_.lengthIs < ROWS_PER_PAGE)) {
+          val newLastPage = pages.last :+ row
+          pages.init :+ newLastPage
+
+        } else pages :+ Vector(row)
+
+      Right(Table(numRows + 1, newPages))
+
+    } else Left(new RuntimeException("exceed page size"))
+
 }
 
 object Table {
 
   type Page = Vector[Row]
 
-  val PAGE_SIZE = 4096
+  val PAGE_SIZE       = 4096
   val TABLE_MAX_PAGES = 100
-  val ROWS_PER_PAGE = PAGE_SIZE / Row.ROW_SIZE
-  val TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES
+  val ROWS_PER_PAGE   = PAGE_SIZE / Row.ROW_SIZE
+  val TABLE_MAX_ROWS  = ROWS_PER_PAGE * TABLE_MAX_PAGES
 
   object Row {
     val ROW_SIZE = 512
