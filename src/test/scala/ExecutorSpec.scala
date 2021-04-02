d745ac87bd8eec1a4a41aa983f3e2aea4f7a2d74
louvre2489@gmail.com <louvre2489@gmail.com>
2021-04-02 18:00:49 +0900

    テスト書いていく準備

---
+++ src/test/scala/ExecutorSpec.scala
@@ -0,0 +1,20 @@
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+import scala.io.StdIn
+
+class ExecutorSpec extends AnyFreeSpec with Matchers {
+//  "add 1 record, select 1 record" in {
+//   Main.runCommand("insert 1 user1 person1@example.com")
+//   Main.runCommand("select")
+//  }
+//  "full record" in {
+//
+//  }
+//  "string too long" in {
+//
+//  }
+//  "negative id is invalid" in {
+//
+//  }
+
+}
